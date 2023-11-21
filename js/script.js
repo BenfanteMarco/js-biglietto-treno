94f7142c1ae9ddd129135b3d3696ec3d05dce1cd
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2023-11-21 15:05:15 +0100

    [FX]

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@ let distance = prompt('Quanto dista la destinazione?'); //km
 let eta = prompt('Quanti anni hai?'); //under18 o over65 sconto
 
 let finalPrice;
-let price = 0.21 * km;
+let price = 0.21 * distance;
 
 if(eta < 18){
-    finalprice = (price * 0.8).toFixed(2);
+    finalPrice = (price * 0.8).toFixed(2);
 }
 else if (eta > 65){
-    finalprice = (price * 0.6).toFixed(2);
+    finalPrice = (price * 0.6).toFixed(2);
 }
 else{
-    finalprice = price.toFixed(2);
+    finalPrice = price.toFixed(2);
 }
 
 console.log(price);
 
-document.getElementById('ticket').innerHTML = finalprice + "€";
+document.getElementById('ticket').innerHTML = finalPrice;
